971817a96a1b68de4a4ba6f2f613f9e8085ae4c0
Marcos Rava <marcosrava@gmail.com>
2021-11-10 11:49:32 -0300

    chore(module): import risk module to app

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InsuranceModule } from './insurance/insurance.module';
+import { RiskModule } from './risk/risk.module';
 
 @Module({
-  imports: [InsuranceModule],
+  imports: [InsuranceModule, RiskModule],
   controllers: [AppController],
   providers: [AppService],
 })
