621773e3663ea181cf8bc0498996538cabbeb6a3
Marcos Rava <marcosrava@gmail.com>
2021-11-15 13:18:43 -0300

    chore(hello-world): Remove hello world

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from 'src/config/config.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { InsuranceModule } from './insurance/insurance.module';
 import { RiskModule } from './risk/risk.module';
 
 @Module({
   imports: [ConfigModule, InsuranceModule, RiskModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
